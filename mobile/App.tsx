abf02fec861dce6d98ef23bf40e77d8c6aa46217
Anna <54253504+ahaydee@users.noreply.github.com>
2022-05-15 14:20:16 -0300

    Extra - Add Mobile and Coverage

---
+++ mobile/App.tsx
@@ -0,0 +1,57 @@
+import 'react-native-gesture-handler';
+
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { theme } from './src/theme';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+import Widget from './src/components/Widget';
+import { useCallback, useEffect, useState } from 'react';
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_500Medium
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, [])
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{flex: 1, backgroundColor: theme.colors.background}}
+      onLayout={onLayoutRootView}
+    >
+      <StatusBar
+        style="light"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Widget />
+    </View>
+  );
+}
